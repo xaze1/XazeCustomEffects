706a0debc1bb4f609e41e8964c12d3f6ff19e6c3
Xaze <eriknachricht@gmail.com>
2025-07-05 14:36:41 +0200

    Added Copyright to files

--- XazeCustomEffects/Features/CustomEffectsController.cs
+++ XazeCustomEffects/Features/CustomEffectsController.cs
@@ -9,6 +9,13 @@ using Mirror.RemoteCalls;
 using PlayerRoles;
 using PlayerRoles.Spectating;
 using UnityEngine;
+// Copyright (c) 2025 xaze_
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+// 
+// I <3 🦈s :3c
+
 using Utils.NonAllocLINQ;
 
 namespace XazeCustomEffects.Features
